759c768147f5603ed9180544b4d0b0e4a8e7c377
Vladimir Mano <bujinkank1@gmail.com>
2023-07-11 13:41:24 -0300

    Exercise

--- Python_101/python_intermediate/02.1_chalenges.py
+++ Python_101/python_intermediate/02.1_chalenges.py
@@ -1,1 +1,25 @@
 ### Resolver desafios usando lo aprendido en python ###
+
+"""
+for i in range(1, 101): ## Rango se utiliza para imprimir numeros
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
+"""
+
+def fizz_b():
+    for index in range(1, 101):
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")
+        else:
+            print(index)
+fizz_b()
