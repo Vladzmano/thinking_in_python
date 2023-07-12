2b924181da7aae56bceddb884d34a4669fe67359
Vladimir Mano <bujinkank1@gmail.com>
2023-07-12 16:52:35 -0300

    Practice

--- Python_101/python_intermediate/02.1_chalenges.py
+++ Python_101/python_intermediate/02.1_chalenges.py
@@ -116,8 +116,10 @@ sin usar funciones propias del lenguaje que lo hagan de forma automatica
 def reverse(text):
     text_len = len(text)
     reversed_text = ""
-    for index in range(0,len(text)):
+    for index in range(0,text_len):
         reversed_text += text[text_len - index -  1]
+        print(reversed_text)
     return reversed_text
 
 print(reverse("Hola mundo"))
+
