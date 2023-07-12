97340363fd756444130f51956a14742d0a41f958
Vladimir Mano <bujinkank1@gmail.com>
2023-07-12 18:27:35 -0300

    File handling

--- Python_101/python_intermediate/05_Error Types.py
+++ Python_101/python_intermediate/05_Error Types.py
@@ -1,9 +1,11 @@
 ### typos - Error Types in Python ###
 
+
 # SyntaxError
 
 #print "Hola comunidad!" # Error
 print("Hola comunidad!")
+
 
 ## NameError ##
 
@@ -11,6 +13,7 @@ print("Hola comunidad!")
 
 language = "Spanish"
 print(language)
+
 
 ## IndexError ##
 
@@ -22,10 +25,12 @@ print(my_list[3])
 # print(my_list[7]) # IndexError: list index out of range
 
 
+
 ## Module not Found Error ##
 
 # import maths # ModuleNotFoundError: No module named 'maths'
 import math
+
 
 
 ## AttributeError ##
@@ -34,6 +39,7 @@ import math
 # print(math.PI) # AttributeError: module 'math' has no attribute 'PI'. Did you mean: 'pi'?
 print(math.pi)
 
+
 ## KeyError ## Los indices debes ser 'int' not 'str'
 
 my_dict = {"Nombre" : "Vlad", "Apellido" : "Mano", "Edad" : 34, 1 : "Python"}
@@ -41,11 +47,13 @@ print(my_dict["Edad"])
 # print(my_dict["Apelido"]) # KeyError: 'Apelido'
 print(my_dict["Apellido"])
 
+
 ## TypeError ##
 
 # print(my_list["0"]) # TypeError: list indices must be integers or slices, not str
 print(my_list[0])
 print(my_list[False])
+
 
 ## ImportError ##
 
@@ -53,3 +61,20 @@ print(my_list[False])
 from math import pi
 print(pi)
 
+
+## ValueError ##
+
+my_int = ("10")
+#my_int = int("10 Anos") # ValueError: invalid literal for int() with base 10: '10 Anos'
+print(int(my_int))
+print(type(my_int))
+
+
+## ZeroDivisionError ##
+
+# print(4/0) ZeroDivisionError: division by zero
+print(4/2)
+
+## File not found
+
+# FileNotFoundError: [Errno 2] No such file or directory: '06.1_my_file.txt'
---
+++ Python_101/python_intermediate/06_File_handling.py
@@ -0,0 +1,29 @@
+### Tipos De ficheros - File handling ###
+
+## .txt file
+
+## Python_101 is the roor path
+## using "r","w+","a", "rb",  we can read and write the file
+
+import os
+
+txt_file = open("Python_101/python_intermediate/06.1_my_file.txt", "w+") # leer y escribir / read adn write
+txt_file.write("File Handling 101\nHello,\nPlease use this file wisely.\nThank you.")
+
+
+#print(txt_file.read())
+#print(txt_file.read(10))
+#print(txt_file.readline()) # lee primera
+#print(txt_file.readline()) # lee segunda
+
+for line in txt_file.readlines(): # lee tercera y cuarta linea.
+    print(line)
+
+txt_file.write("\nPS: Please bring water XOXOX")
+print(txt_file.readline())
+
+txt_file.close()
+
+#os.remove("Python_101/python_intermediate/06.1_my_file.txt")
+
+## .jason file
