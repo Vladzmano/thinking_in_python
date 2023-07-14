914fc742d29fd9f0ec6958b08af96d2522b0b7e3
Vladimir Mano <bujinkank1@gmail.com>
2023-07-14 16:59:39 -0300

    Regular expressions 101

--- Python_101/python_intermediate/07_regular_expressions.py
+++ Python_101/python_intermediate/07_regular_expressions.py
@@ -5,7 +5,7 @@
 
 import re
 
-my_string = "Esta es la leccion numero 7: Expresiones Regulares"
+my_string = "Esta es la leccion numero 7: leccion llamada Expresiones Regulares"
 my_other_string = "Esta no es la leccion numero 6 : Manejo de ficheros"
 
 match = (re.match("Esta es la leccion", my_string, re.I))
@@ -51,3 +51,38 @@ print(re.split(":",my_string))
 
 print(re.sub("[l|L]]eccion", "LECCION", my_string))
 print(re.sub("Expresiones Regulares", "RegEx", my_string))
+
+
+## patters (or custom patters) ## 'r' es para algo reservado de python
+
+pattern = r"[l|L]eccion"
+print(re.findall(pattern, my_string))
+
+pattern =r"[l|L]eccion|Expresiones"
+print(re.findall(pattern,my_string))
+
+pattern = r"[0-9]"
+print(re.findall(pattern, my_string))
+print(re.search(pattern, my_string))
+
+pattern = r"\d"
+print(re.findall(pattern, my_string))
+
+pattern = r"\D"
+print(re.findall(pattern, my_string))
+
+
+pattern = r"[l].*" # periodo y conjunto
+print(re.findall(pattern, my_string))
+
+
+## email valiadtion / check ##
+
+email = "vladmano@vlad.com"
+pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-Z0-9-.]+$"# '^' = empieza por. '$' toma en cuenta el resto del texto o numeros del listado
+print(re.match(pattern, email))
+print(re.search(pattern, email))
+print(re.findall(pattern, email))
+
+email = "vladmano@vlad"
+print(re.findall(pattern, email))
